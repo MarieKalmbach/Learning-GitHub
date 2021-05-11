46fce9297549c142cd49df04693c3f8ceb769bef
Marie Kalmbach <Marie.Kalmbach@gmail.com>
2021-05-11 14:08:59 -0500

    Initial Commit

---
+++ LearningGitHub/LearningGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGitHubApp.swift
+//  LearningGitHub
+//
+//  Created by BPSTIL-Pascal on 5/11/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
